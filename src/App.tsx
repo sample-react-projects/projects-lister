087c2670599fd27ae110c19857a1211d7eaaea07
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-01-10 23:20:13 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styles from "./App.module.scss";
+import AddProject from "./components/add-project/AddProject";
 import Navigation from "./components/navigation/Navigation";
 
 function App() {
@@ -7,7 +8,9 @@ function App() {
       <div className={styles["container__rail-left"]}>
         <Navigation></Navigation>
       </div>
-      <div className={styles["container__rail-right"]}>Right</div>
+      <div className={styles["container__rail-right"]}>
+        <AddProject></AddProject>
+      </div>
     </div>
   );
 }
