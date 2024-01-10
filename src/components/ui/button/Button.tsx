53a29029b7769816b2321a57a11a83d7baeac99f
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-01-11 02:15:15 +0530

    Un-enforce click event

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -1,26 +1,20 @@
-import { MouseEventHandler, PropsWithChildren } from "react";
+import { PropsWithChildren } from "react";
 import styles from "./Button.module.scss";
 import { Variant } from "./Button.variant";
 
 interface IButton extends PropsWithChildren {
-  className?: string;
-  onClick: MouseEventHandler<HTMLButtonElement>;
   variant: Variant;
+  [x: string]: any;
 }
 
 const Button: React.FC<IButton> = ({
   children,
   className = "",
-  onClick,
   variant,
   ...props
 }) => {
   return (
-    <button
-      onClick={onClick}
-      className={`${className} ${styles[variant]}`}
-      {...props}
-    >
+    <button className={`${className} ${styles[variant]}`} {...props}>
       {children}
     </button>
   );
