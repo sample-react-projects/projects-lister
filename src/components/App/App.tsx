21893cb6f15d5c89f5bc152676e6f60ceec8a0c8
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-01-11 12:46:31 +0530

    Use context provider

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,19 @@
 import styles from "./App.module.scss";
 import AddProject from "../add-project/AddProject";
 import Navigation from "../navigation/Navigation";
+import ProjectContextProvider from "../../context/ProjectsContextProvider";
 
 function App() {
   return (
     <div className={styles.container}>
-      <div className={styles["container__rail-left"]}>
-        <Navigation></Navigation>
-      </div>
-      <div className={styles["container__rail-right"]}>
-        <AddProject></AddProject>
-      </div>
+      <ProjectContextProvider>
+        <div className={styles["container__rail-left"]}>
+          <Navigation></Navigation>
+        </div>
+        <div className={styles["container__rail-right"]}>
+          <AddProject></AddProject>
+        </div>
+      </ProjectContextProvider>
     </div>
   );
 }
