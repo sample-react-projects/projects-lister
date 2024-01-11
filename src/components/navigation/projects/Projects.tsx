f4ed005012fd73014f88d03bb005c5a258262438
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-01-11 13:31:11 +0530

    Create projects component

---
+++ src/components/navigation/projects/Projects.tsx
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import { ProjectsContext } from "../../../context/ProjectsContextProvider";
+import styles from "./Projects.module.scss";
+
+const Projects: React.FC<{}> = () => {
+  const { getProjects, getActiveProjectId } = useContext(ProjectsContext);
+  const projects = getProjects();
+  const activeProjectId = getActiveProjectId();
+
+  return (
+    <ul className={styles.projects}>
+      {projects.map((project) => {
+        return (
+          <li key={project.id}
+            className={`${styles.project} ${
+              activeProjectId === project.id ? styles.active : null
+            }`}
+          >
+            {project.title}
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default Projects;
