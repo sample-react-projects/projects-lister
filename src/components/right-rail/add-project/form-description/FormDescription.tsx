12cc0d46b0a5abcb85b8c2c8d36b0f9ce4e2c350
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-01-11 20:11:15 +0530

    Fix form stylees

--- src/components/right-rail/add-project/form-description/FormDescription.tsx
+++ src/components/right-rail/add-project/form-description/FormDescription.tsx
@@ -1,6 +1,6 @@
 const FormDescription = () => {
   return (
-    <>
+    <div>
       <h2>How it works?</h2>
       <p>
         You may start by adding projects with the help of the form below which
@@ -10,7 +10,7 @@ const FormDescription = () => {
         Once added, you may select that project and start adding subtasks to it.
       </p>
       <p>Subtask done? Just delete it from the list!</p>
-    </>
+    </div>
   );
 };
 
