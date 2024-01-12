c0c9b2ee29d62827e2825f7b8ea956ff6767c4fd
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-01-12 19:56:45 +0530

    Use header class

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,8 +2,10 @@ import styles from "./Header.module.scss";
 
 const Header: React.FC<{}> = () => {
   return (
-    <header>
-      <div className={styles.content}><strong>Projects Lister</strong></div>
+    <header className={styles.header}>
+      <div className={styles.content}>
+        <strong>Projects Lister</strong>
+      </div>
     </header>
   );
 };
