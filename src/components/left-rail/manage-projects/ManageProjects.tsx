c62480187a9fa489fc75f96496377db159ddc7de
Saksham Malhotra <saksham.malhotra@openlane.com>
2024-01-19 01:47:38 +0530

    Move left rail into manage projects

---
+++ src/components/left-rail/manage-projects/ManageProjects.tsx
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import Button from "../../ui/button/Button";
+import { Variant } from "../../ui/button/Button.variant";
+import {
+  IProjectsContext,
+  ProjectsContext,
+} from "../../../context/ProjectsContextProvider";
+import Projects from "../projects/Projects";
+
+const ManageProjects: React.FC<{}> = () => {
+  const { setActiveProjectId } = useContext<IProjectsContext>(ProjectsContext);
+
+  function resetActiveProjectId() {
+    setActiveProjectId("");
+  }
+
+  return (
+    <div className="container--vertical">
+      <h2>My Projects</h2>
+      <Projects></Projects>
+      <Button
+        style={{ alignSelf: "flex-start" }}
+        variant={Variant.primary}
+        onClick={resetActiveProjectId}
+      >
+        + Add Project
+      </Button>
+    </div>
+  );
+};
+
+export default ManageProjects;
